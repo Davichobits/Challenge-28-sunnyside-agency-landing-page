051c0305447c0398410b64f760b6175a611b9185
Davichobits <davidruizdeveloper@gmail.com>
2023-07-05 05:00:23 -0500

    navbar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import { Nav } from './Nav'
 export const Header = () => {
   return (
     <header className='absolute w-full px-6 pt-8 '>
-      <div className='flex h-[24px] place-content-between m-auto'>
-        <img className='w-[124px] h-6 xl:w-[170px] xl:h-[33px]' src={logo} alt="Logo sunnyside" />
+      <div className='flex h-6 place-content-between m-auto sm:h-14 items-center'>
+        <img className='w-[124px] h-6 sm:w-[170px] sm:h-[33px]' src={logo} alt="Logo sunnyside" />
         <Nav />
       </div>
     </header>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,13 +12,19 @@ export const Nav = () => {
   console.log(menuClicked)
   return (
     <nav className='font-Barlow'>
-      <ul className={`${menuClicked ? 'block' : 'hidden' } sm:block bg-white w-[329px] h-[305px] absolute top-[106px] left-0 right-0 mx-auto z-10 flex flex-col place-content-between py-[39px] items-center text-Dark-grayish-blue text-xl after:content-[""] after:absolute after:top-[-24px] after:right-0 after:border-[12px] after:border-white after:border-t-transparent after:border-l-transparent`}>
+      <ul className={`${menuClicked ? 'flex' : 'hidden' } bg-white w-[329px] h-[305px] absolute top-[106px] left-0 right-0 mx-auto z-10 flex-col place-content-between py-[39px] items-center text-Dark-grayish-blue text-xl after:content-[""] after:absolute after:top-[-24px] after:right-0 after:border-[12px] after:border-white after:border-t-transparent after:border-l-transparent 
+      
+      sm:after:hidden sm:bg-transparent sm:relative sm:flex sm:flex-row sm:h-full sm:top-0 sm:w-[400px] sm:py-0 sm:text-White 
+      
+      lg:w-[490px]`}>
         <li className='cursor-pointer'>About</li>
         <li className='cursor-pointer'>Services</li>
         <li className='cursor-pointer'>Projects</li>
-        <li className='cursor-pointer bg-Yellow  w-[140px] h-[56px] grid place-content-center rounded-full text-Very-dark-desaturated-blue uppercase font-Fraunces'>Contact</li>
+        <li className='cursor-pointer bg-Yellow  w-[140px] h-[56px] grid place-content-center rounded-full text-Very-dark-desaturated-blue uppercase font-Fraunces 
+        
+        sm:bg-White sm:hover:text-white sm:hover:bg-opacity-25'>Contact</li>
       </ul>
-      <div className='cursor-pointer' onClick={handleClick}>
+      <div className='cursor-pointer sm:hidden' onClick={handleClick}>
         <img src={burgerMenu} alt="menu hamburger" />
       </div>
     </nav>
